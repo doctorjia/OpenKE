f41dc9cb5185a66631c9a876854c329217e2fc71
jiazhifeng <jiazhifeng@sensetime.com>
2021-03-22 12:38:52 +0800

    Updates in experiment script

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python train_simple_WN18RR.py ./benchmarks/WN18RR/
 python ./openke/data/data_process.py
-python ./benchmarks/WN18RR_1/n-n.py 141442 5000 5000
+python ./benchmarks/WN18RR_1/n-n.py
 python train_simple_WN18RR.py ./benchmarks/WN18RR_1 1
