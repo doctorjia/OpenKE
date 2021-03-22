ca70498cb570927b8a90969dcabe31bce7fbc62d
jiazhifeng <jiazhifeng@sensetime.com>
2021-03-22 12:38:41 +0800

    Updates in experiment script

--- examples/train_simple_WN18RR.py
+++ examples/train_simple_WN18RR.py
@@ -8,14 +8,14 @@ from openke.data import TrainDataLoader, TestDataLoader
 
 # dataloader for training
 train_dataloader = TrainDataLoader(
-	in_path = sys.argv[1],
-	nbatches = 100,
-	threads = 8, 
-	sampling_mode = "normal", 
-	bern_flag = 1, 
-	filter_flag = 1, 
-	neg_ent = 25,
-	neg_rel = 0
+    in_path=sys.argv[1],
+    nbatches=100,
+    threads=8,
+    sampling_mode="normal",
+    bern_flag=1,
+    filter_flag=1,
+    neg_ent=25,
+    neg_rel=0
 )
 
 # dataloader for test
@@ -23,39 +23,40 @@ test_dataloader = TestDataLoader(sys.argv[1], "link")
 
 # define the model
 simple = SimplE(
-	ent_tot = train_dataloader.get_ent_tot(),
-	rel_tot = train_dataloader.get_rel_tot(),
-	dim = 200
+    ent_tot=train_dataloader.get_ent_tot(),
+    rel_tot=train_dataloader.get_rel_tot(),
+    dim=200
 )
 
 # define the loss function
 model = NegativeSampling(
-	model = simple, 
-	loss = SoftplusLoss(),
-	batch_size = train_dataloader.get_batch_size(), 
-	regul_rate = 1.0
+    model=simple,
+    loss=SoftplusLoss(),
+    batch_size=train_dataloader.get_batch_size(),
+    regul_rate=1.0
 )
-
 
 # train the model
 if len(sys.argv) == 2:
-	trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 500, alpha = 0.5, use_gpu = True, opt_method = "adagrad")
-	trainer.run()
-	simple.save_checkpoint('./checkpoint/simple.ckpt')
-	simple.save_parameters('./parameters/simple.json')
+    trainer = Trainer(model=model, data_loader=train_dataloader, train_times=500, alpha=0.5, use_gpu=True,
+                      opt_method="adagrad")
+    trainer.run()
+    simple.save_checkpoint('./checkpoint/simple.ckpt')
+    simple.save_parameters('./parameters/simple.json')
 elif len(sys.argv) == 3:
-	simple.load_checkpoint('./checkpoint/simple.ckpt')
-	trainer = Trainer(model = simple, data_loader = train_dataloader, train_times = 200, alpha = 0.5, use_gpu = True, opt_method = "adagrad")
-	trainer.run()
-	simple.save_checkpoint('./checkpoint/simple'+sys.argv[2]+'.ckpt')
-	simple.save_parameters('./parameters/simple'+sys.argv[2]+'.json')
+    simple.load_checkpoint('./checkpoint/simple.ckpt')
+    trainer = Trainer(model=simple, data_loader=train_dataloader, train_times=200, alpha=0.5, use_gpu=True,
+                      opt_method="adagrad")
+    trainer.run()
+    simple.save_checkpoint('./checkpoint/simple' + sys.argv[2] + '.ckpt')
+    simple.save_parameters('./parameters/simple' + sys.argv[2] + '.json')
 
 # test the model
 if len(sys.argv) == 2:
-	simple.load_checkpoint('./checkpoint/simple.ckpt')
-	tester = Tester(model = simple, data_loader = test_dataloader, use_gpu = True)
-	tester.run_link_prediction(type_constrain = False)
+    simple.load_checkpoint('./checkpoint/simple.ckpt')
+    tester = Tester(model=simple, data_loader=test_dataloader, use_gpu=True)
+    tester.run_link_prediction(type_constrain=False)
 elif len(sys.argv) == 3:
-	simple.load_checkpoint('./checkpoint/simple'+sys.argv[1]+'.ckpt')
-	tester = Tester(model=simple, data_loader=test_dataloader, use_gpu=True)
-	tester.run_link_prediction(type_constrain=False)
+    simple.load_checkpoint('./checkpoint/simple' + sys.argv[1] + '.ckpt')
+    tester = Tester(model=simple, data_loader=test_dataloader, use_gpu=True)
+    tester.run_link_prediction(type_constrain=False)
--- openke/data/data_process.py
+++ openke/data/data_process.py
@@ -6,7 +6,7 @@ import shutil
 
 # default_repo = '/export/data/zjiaad/OpenKE/benchmarks/WN18RR/'
 default_repo = '../../benchmarks/WN18RR/'
-out_repo = '../../benchmarks/WN18RR_1'
+out_repo = '../../benchmarks/WN18RR_1/'
 
 entity_dict_file = default_repo + 'entity2id.txt'
 relation_dict_file = default_repo + 'relation2id.txt'
@@ -33,25 +33,41 @@ training_triples = list(zip([entity_dict[h] for h in training_df[0]],
                             [entity_dict[t] for t in training_df[1]],
                             [relation_dict[r] for r in training_df[2]]))
 n_training_triple = len(training_triples)
-np.savetxt(default_repo + "train2id.txt", training_triples, fmt='%d', delimiter=' ')
-shutil.copyfile(default_repo + "train2id.txt", out_repo + "train2id.txt")
+with open(default_repo + "train2id.txt", "w") as f:
+    f.write(str(n_training_triple)+'\n')
+f.close()
+with open(default_repo + "train2id.txt", "ab") as f:
+    np.savetxt(f, training_triples, fmt='%d', delimiter=' ')
+f.close()
 print('#training triple: {}'.format(n_training_triple))
+
 print('-----Loading validation triples-----')
 validation_df = pd.read_table(validation_file, header=None)
 validation_triples = list(zip([entity_dict[h] for h in validation_df[0]],
                               [entity_dict[t] for t in validation_df[1]],
                               [relation_dict[r] for r in validation_df[2]]))
 n_validation_triple = len(validation_triples)
-np.savetxt(default_repo + "valid2id.txt", validation_triples, fmt='%d', delimiter=' ')
+with open(default_repo + "valid2id.txt", "w") as f:
+    f.write(str(n_validation_triple)+'\n')
+f.close()
+with open(default_repo + "valid2id.txt", "ab") as f:
+    np.savetxt(f, validation_triples, fmt='%d', delimiter=' ')
+f.close()
 shutil.copyfile(default_repo + "valid2id.txt", out_repo + "valid2id.txt")
 print('#validation triple: {}'.format(n_validation_triple))
+
 print('-----Loading test triples------')
 test_df = pd.read_table(test_file, header=None)
 test_triples = list(zip([entity_dict[h] for h in test_df[0]],
                         [entity_dict[t] for t in test_df[1]],
                         [relation_dict[r] for r in test_df[2]]))
 n_test_triple = len(test_triples)
-np.savetxt(default_repo + "test2id.txt", test_triples, fmt='%d', delimiter=' ')
+with open(default_repo + "test2id.txt", "w") as f:
+    f.write(str(n_test_triple)+'\n')
+f.close()
+with open(default_repo + "test2id.txt", "ab") as f:
+    np.savetxt(f, test_triples, fmt='%d', delimiter=' ')
+f.close()
 shutil.copyfile(default_repo + "test2id.txt", out_repo + "test2id.txt")
 
 i = 0
@@ -61,10 +77,27 @@ relation_id = 17
 
 if os.path.exists(out_repo + "train.txt"):
     os.remove(out_repo + "train.txt")
-shutil.copyfile(default_repo + "train.txt", out_repo + "train.txt")
+# shutil.copyfile(default_repo + "train.txt", out_repo + "train.txt")
+with open(default_repo+"train.txt", "r") as f1:
+    lines_default = f1.readlines()
+lines_default[0] = lines_default[0].replace(lines_default[0], "141455\n")
+with open(out_repo+"train.txt", "w") as f2:
+    for line in lines_default:
+        f2.write(line)
+f1.close()
+f2.close()
+
 if os.path.exists(out_repo + "entity2id.txt"):
     os.remove(out_repo + "entity2id.txt")
-shutil.copyfile(default_repo + "entity2id.txt", out_repo + "entity2id.txt")
+# shutil.copyfile(default_repo + "entity2id.txt", out_repo + "entity2id.txt")
+with open(default_repo+"entity2id.txt", "r") as f1:
+    lines_default = f1.readlines()
+lines_default[0] = lines_default[0].replace(lines_default[0], "40948\n")
+with open(out_repo+"entity2id.txt", "w") as f2:
+    for line in lines_default:
+        f2.write(line)
+f1.close()
+f2.close()
 
 output_file = open(out_repo + "train.txt", "a")
 entity_file = open(out_repo + "entity2id.txt", "a")
@@ -89,12 +122,19 @@ while i < 5:
 
 output_file.close()
 
-training_file = out_repo + 'train.txt'
+training_file = open(out_repo + 'train.txt', 'r')
 print('-----Loading training triples-----')
-training_df = pd.read_table(training_file, header=None)
-training_triples_2 = list(zip([entity_dict[h] for h in training_df[0]],
-                              [entity_dict[t] for t in training_df[1]],
-                              [relation_dict[r] for r in training_df[2]]))
-np.savetxt(out_repo + "train2id.txt", training_triples_2, fmt='%d', delimiter=' ')
-n_training_triple = len(training_triples)
-print('#training triple: {}'.format(n_training_triple))
+tot = (int)(training_file.readline())
+training_df = pd.read_csv(training_file, header=0, sep='\t')
+training_df.columns = ["A","B","C"]
+training_triples_2 = list(zip([entity_dict[h] for h in training_df["A"]],
+                              [entity_dict[t] for t in training_df["B"]],
+                              [relation_dict[r] for r in training_df["C"]]))
+n_training_new = len(training_triples_2)
+with open(out_repo + "train2id.txt", "w") as f:
+    f.write(str(n_training_new)+'\n')
+f.close()
+with open(out_repo + "train2id.txt", "ab") as f:
+    np.savetxt(f, training_triples_2, fmt='%d', delimiter=' ')
+f.close()
+print('#training triple: {}'.format(n_training_new))
