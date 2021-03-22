3a84f6519ca92dfc31c3377c74c6ab127a65cf00
jiazhifeng <jiazhifeng@sensetime.com>
2021-03-22 10:46:38 +0800

    Updates in experiment script

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 current=`date "+%Y-%m-%d %H:%M:%S"`
 timeStamp=`date -d "$current" +%s`
 currentTimeStamp=$((timeStamp+`date "+%N"`/1000))
-train_simple_WN18RR.py ./benchmarks/WN18RR/
-./openke/data/data_process.py
-./benchmarks/WN18RR_1/n-n.py
-train_simple_WN18RR.py ./benchmarks/WN18RR_1 currentTimeStamp
+python train_simple_WN18RR.py ./benchmarks/WN18RR/
+python ./openke/data/data_process.py
+python ./benchmarks/WN18RR_1/n-n.py
+python train_simple_WN18RR.py ./benchmarks/WN18RR_1 currentTimeStamp
