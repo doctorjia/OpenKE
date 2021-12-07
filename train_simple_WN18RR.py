46d01abc5e97c6c65541a47820445573dfa464a5
doctorjia <double_attack@126.com>
2021-12-07 19:51:49 +0800

    Update codes

--- train_simple_WN18RR.py
+++ train_simple_WN18RR.py
@@ -10,7 +10,7 @@ from openke.data import TrainDataLoader, TestDataLoader
 # dataloader for training
 def load_data(timestamp):
     train_dataloader = TrainDataLoader(
-        in_path = "./benchmarks/archive_" + str(timestamp),
+        in_path = "./benchmarks/archive_" + str(timestamp) + "/",
         nbatches = 100,
         threads = 8,
         sampling_mode = "normal",
@@ -21,7 +21,7 @@ def load_data(timestamp):
     )
 
     # dataloader for test
-    test_dataloader = TestDataLoader("./benchmarks/archive_" + str(timestamp), "link")
+    test_dataloader = TestDataLoader("./benchmarks/archive_" + str(timestamp) + "/", "link")
 
     # define the model
     simple = SimplE(
@@ -42,7 +42,7 @@ def load_data(timestamp):
 
 
 # train the model
-train_dataloader_0, test_dataloader_0, simple_0, model_0 = load_data("0")
+train_dataloader_0, test_dataloader_0, simple_0, model_0 = load_data(0)
 trainer = Trainer(model = model_0, data_loader = train_dataloader_0, train_times = 500, alpha = 0.5, use_gpu = True,
                   opt_method = "adagrad")
 trainer.run()
