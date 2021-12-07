193562f52c31998469f344e3281eaa834828f40d
doctorjia <double_attack@126.com>
2021-12-07 19:38:19 +0800

    Update codes

--- train_simple_WN18RR.py
+++ train_simple_WN18RR.py
@@ -6,57 +6,72 @@ from openke.module.loss import SoftplusLoss
 from openke.module.strategy import NegativeSampling
 from openke.data import TrainDataLoader, TestDataLoader
 
+
 # dataloader for training
-train_dataloader = TrainDataLoader(
-    in_path=sys.argv[1],
-    nbatches=100,
-    threads=8,
-    sampling_mode="normal",
-    bern_flag=1,
-    filter_flag=1,
-    neg_ent=25,
-    neg_rel=0
-)
+def load_data(timestamp):
+    train_dataloader = TrainDataLoader(
+        in_path = "./benchmarks/archive_" + str(timestamp),
+        nbatches = 100,
+        threads = 8,
+        sampling_mode = "normal",
+        bern_flag = 1,
+        filter_flag = 1,
+        neg_ent = 25,
+        neg_rel = 0
+    )
 
-# dataloader for test
-test_dataloader = TestDataLoader(sys.argv[1], "link")
+    # dataloader for test
+    test_dataloader = TestDataLoader("./benchmarks/archive" + str(timestamp), "link")
 
-# define the model
-simple = SimplE(
-    ent_tot=train_dataloader.get_ent_tot(),
-    rel_tot=train_dataloader.get_rel_tot(),
-    dim=200
-)
+    # define the model
+    simple = SimplE(
+        ent_tot = train_dataloader.get_ent_tot(),
+        rel_tot = train_dataloader.get_rel_tot(),
+        dim = 200
+    )
 
-# define the loss function
-model = NegativeSampling(
-    model=simple,
-    loss=SoftplusLoss(),
-    batch_size=train_dataloader.get_batch_size(),
-    regul_rate=1.0
-)
+    # define the loss function
+    model = NegativeSampling(
+        model = simple,
+        loss = SoftplusLoss(),
+        batch_size = train_dataloader.get_batch_size(),
+        regul_rate = 1.0
+    )
+
+    return train_dataloader, test_dataloader, simple, model
+
 
 # train the model
 if len(sys.argv) == 2:
-    trainer = Trainer(model=model, data_loader=train_dataloader, train_times=500, alpha=0.5, use_gpu=True,
-                      opt_method="adagrad")
+    train_dataloader_0, test_dataloader_0, simple_0, model_0 = load_data("0")
+    trainer = Trainer(model = model_0, data_loader = train_dataloader_0, train_times = 500, alpha = 0.5, use_gpu = True,
+                      opt_method = "adagrad")
     trainer.run()
-    simple.save_checkpoint('./checkpoint/simple.ckpt')
-    simple.save_parameters('./parameters/simple.json')
-elif len(sys.argv) == 3:
-    simple.load_checkpoint('./checkpoint/simple.ckpt')
-    trainer = Trainer(model=simple, data_loader=train_dataloader, train_times=200, alpha=0.5, use_gpu=True,
-                      opt_method="adagrad")
-    trainer.run()
-    simple.save_checkpoint('./checkpoint/simple' + sys.argv[2] + '.ckpt')
-    simple.save_parameters('./parameters/simple' + sys.argv[2] + '.json')
+    simple_0.save_checkpoint('./checkpoint/simple_0.ckpt')
+    simple_0.save_parameters('./parameters/simple_0.json')
+    simple_0.load_checkpoint('./checkpoint/simple_0.ckpt')
+    tester = Tester(model = simple_0, data_loader = test_dataloader_0, use_gpu = True)
+    tester.run_link_prediction(type_constrain = False)
+
+    for i in range(5):
+        train_dataloader_now, test_dataloader_now, simple_now, model_now = load_data(str(i + 1))
+        simple_now.load_checkpoint('./checkpoint/simple_' + str(i) + '.ckpt')
+        trainer = Trainer(model = simple_now, data_loader = train_dataloader_now, train_times = 200, alpha = 0.5, use_gpu = True,
+                          opt_method = "adagrad")
+        trainer.run()
+        simple_now.save_checkpoint('./checkpoint/simple' + str(i) + '.ckpt')
+        simple_now.save_parameters('./parameters/simple' + str(i) + '.json')
+
+        simple_now.load_checkpoint('./checkpoint/simple.ckpt')
+        tester = Tester(model = simple_now, data_loader = test_dataloader_now, use_gpu = True)
+        tester.run_link_prediction(type_constrain = False)
 
 # test the model
-if len(sys.argv) == 2:
-    simple.load_checkpoint('./checkpoint/simple.ckpt')
-    tester = Tester(model=simple, data_loader=test_dataloader, use_gpu=True)
-    tester.run_link_prediction(type_constrain=False)
-elif len(sys.argv) == 3:
-    simple.load_checkpoint('./checkpoint/simple' + sys.argv[1] + '.ckpt')
-    tester = Tester(model=simple, data_loader=test_dataloader, use_gpu=True)
-    tester.run_link_prediction(type_constrain=False)
+# if len(sys.argv) == 2:
+#     simple.load_checkpoint('./checkpoint/simple.ckpt')
+#     tester = Tester(model = simple, data_loader = test_dataloader, use_gpu = True)
+#     tester.run_link_prediction(type_constrain = False)
+# elif len(sys.argv) == 3:
+#     simple.load_checkpoint('./checkpoint/simple' + sys.argv[1] + '.ckpt')
+#     tester = Tester(model = simple, data_loader = test_dataloader, use_gpu = True)
+#     tester.run_link_prediction(type_constrain = False)
