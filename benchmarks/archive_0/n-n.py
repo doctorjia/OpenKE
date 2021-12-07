80d0f993058b14715fc7b49848f2e292dd92e9c5
doctorjia <double_attack@126.com>
2021-12-07 15:18:39 +0800

    Update codes

--- benchmarks/archive_0/n-n.py
+++ benchmarks/archive_0/n-n.py
@@ -3,9 +3,9 @@ rig = {}
 rellef = {}
 relrig = {}
 
-triple = open("train2id_0.txt", "r")
-valid = open("valid2id_0.txt", "r")
-test = open("test2id_0.txt", "r")
+triple = open("train2id.txt", "r")
+valid = open("valid2id.txt", "r")
+test = open("test2id.txt", "r")
 
 tot = (int)(triple.readline())
 for i in range(tot):
@@ -99,7 +99,7 @@ s11=0
 s1n=0
 sn1=0
 snn=0
-f = open("test2id_0.txt", "r")
+f = open("test2id.txt", "r")
 tot = (int)(f.readline())
 for i in range(tot):
 	content = f.readline()
@@ -117,12 +117,12 @@ for i in range(tot):
 f.close()
 
 
-f = open("test2id_0.txt", "r")
+f = open("test2id.txt", "r")
 f11 = open("1-1.txt", "w")
 f1n = open("1-n.txt", "w")
 fn1 = open("n-1.txt", "w")
 fnn = open("n-n.txt", "w")
-fall = open("test2id_0_all.txt", "w")
+fall = open("test2id_all.txt", "w")
 tot = (int)(f.readline())
 fall.write("%d\n"%(tot))
 f11.write("%d\n"%(s11))
